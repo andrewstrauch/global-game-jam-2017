811d01f9302e73e7b63d8e2b6ec5698937477ef9
Andrew Strauch <rememberx@gmail.com>
2017-01-20 16:11:30 -0500

    Controller as singleton. Set Main.tscn as default scene. Added base Map node, camera, and loadTimer

--- Controller.gd
+++ Controller.gd
@@ -1,8 +1,14 @@
-
 extends Node
 
 
 
 func _ready():
+	#var _root=get_tree().get_root()
+	#root = _root.get_child(_root.get_child_count()-1)
+	#map_layer = root.get_node("Map")
+	#camera = root.get_node("Map/Camera")
+	#camera.make_current()
+	#set_fixed_process(true)
+	#OS.set_window_fullscreen(true)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	pass
-
