798859412b9d46594e4a170082d173c27c164a99
Austin DeVinney <onikami@gmail.com>
2017-01-21 10:38:04 -0500

    Expand on base enemy class

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -1,15 +1,30 @@
 extends Node2D
 
 # Base class for Enemy
-var hp = 0
-var position = Vector2(1.0, 1.0)
+export var max_hp = 1
+export var move_speed = 1
+
+var hp
 
 # Init
 func _ready():
+	hp = max_hp # Init the current HP to the max
 	set_fixed_process(true) # Call every physics update
 	pass
 
 # Physics step
 func _fixed_process(delta):
 	# Process shit
+	pass
+
+# Generic function for taking damage
+func takeDamage(damage):
+	hp -= damage
+	if(hp <= 0):
+		destroy()
+	pass
+
+# Destroy function to remove enemy and do any other cleanup
+func destroy():
+	queue_free()
 	pass
