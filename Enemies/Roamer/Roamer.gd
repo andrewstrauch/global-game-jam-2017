ae9ec6b7159fac62e0294c53ca553531db71f022
Andrew Strauch <rememberx@gmail.com>
2017-01-21 18:13:55 -0500

    Finished Roamer logic.

--- Enemies/Roamer/Roamer.gd
+++ Enemies/Roamer/Roamer.gd
@@ -7,16 +7,33 @@ func _ready():
 	_generate_direction()
 
 func _fixed_process(delta):
-	pass
-	#move in direction
+	var motion = Vector2()
+	
+	if (currentDirection == Vector2(-1, 0)): #left
+		motion += currentDirection
+	if (currentDirection == Vector2(0, -1)): #up
+		motion += currentDirection
+	if (currentDirection == Vector2(1, 0)): #right
+		motion += currentDirection
+	if (currentDirection == Vector2(1, 0)): #down
+		motion += currentDirection
+	
+	motion = motion.normalized()*MOTION_SPEED*delta
+	move(motion)
 
 func _change_timer_timeout():
 	_generate_direction()
 
 func _generate_direction():
 	var dir = randi()%4+1
-#	if (dir == 1):
-#		currentDirection = -1, 0
+	if (dir == 1): #left
+		currentDirection = Vector2(-1, 0)
+	if (dir == 2): #up
+		currentDirection = Vector2(0, -1)
+	if (dir == 3): #right
+		currentDirection = Vector2(1, 0)
+	if (dir == 4): #down
+		currentDirection = Vector2(1, 0)
 		
 	get_node("ChangeDirectionTimer").set_one_shot(true)
 	get_node("ChangeDirectionTimer").start()
