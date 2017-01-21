60b8339d2932047a6caff3a7a85da67a6f6bb32c
Andrew Strauch <rememberx@gmail.com>
2017-01-21 10:28:34 -0500

    Player bullet firing

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,10 +3,19 @@ extends KinematicBody2D
 const MOTION_SPEED = 180 # Pixels/second
 var health = 3
 var controller
+var isPullingTrigger = false
+var weaponCanFire = bool (true)
+var isPlayerFacingRight = false
+
 
 func _ready():
 	controller = get_node("/root/Controller")
 	controller.cam_target = self
+	get_node("WeaponReloadTimer").set_one_shot(true)
+	weaponCanFire = true
+	get_node("WeaponReloadTimer").connect("timeout", self, "_weapon_reload_timer_timeout")
+	#get_node("hitbox").connect("body_enter", self, "_on_hitbox_body_enter")
+	
 	set_fixed_process(true)
 
 func _fixed_process(delta):
@@ -24,6 +33,12 @@ func _fixed_process(delta):
 	motion = motion.normalized()*MOTION_SPEED*delta
 	move(motion)
 	
+	isPullingTrigger = Input.is_key_pressed(KEY_SPACE)
+	
+	if (isPullingTrigger && weaponCanFire):
+			weaponCanFire = false
+			_weapon_shoot(motion)
+	
 func reset_health():
 	health = 3
 	get_node("./HeartbeatPlayer").stop_heartbeat()
@@ -34,3 +49,16 @@ func decrement_health():
 		get_node("./HeartbeatPlayer").play_slow_heartbeat()
 	elif (health == 1):
 		get_node("./HeartbeatPlayer").play_fast_heartbeat()
+
+func _weapon_shoot(motion):
+	get_node("WeaponReloadTimer").set_wait_time(.3)
+	get_node("WeaponReloadTimer").start()
+	var bullet = load("res://Player/PlayerBullet.tscn")
+	var bi = bullet.instance()
+	get_tree().get_root().add_child(bi)
+	bi.set_pos(get_pos() + Vector2(20, 20))
+	#bi.init(motion)
+	bi.init(isPlayerFacingRight)
+
+func _weapon_reload_timer_timeout():
+    weaponCanFire = true
---
+++ Player/PlayerBullet.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+var isBulletFacingRight
+var direction = int()
+var speed = 14
+#var hurtful_type = preload("res://Enemies/HurtfulToPlayer.gd")
+var enemy_class = preload("res://Enemies/Enemy.gd")
+var tiles = preload("res://Scenes/Tiles1.res")
+#var cube = preload("res://Enemies/Cube.gd")
+
+func _ready():
+	get_node("Hitbox").connect("body_enter", self, "_on_hitbox_body_enter")
+	set_fixed_process(true)
+	
+func init(incomingBool):
+	isBulletFacingRight = incomingBool
+	if(isBulletFacingRight == true):
+		direction = 1
+	else:
+		direction = -1
+
+func _fixed_process(delta):
+	var pos = get_pos() + Vector2(speed * direction, 0)
+	set_pos(pos)
+
+func _on_hitbox_body_enter( body ):
+	if(body extends enemy_class):
+		#kill enemy
+		_death()
+	if(body extends TileMap):
+		_death()
+func _death():
+	queue_free()
