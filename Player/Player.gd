00ac06018eac79c39fd595d9fd711dd0cdcf57a0
Alex Meade <mr.alex.meade@gmail.com>
2017-01-21 13:40:46 -0500

    Add player bump shockwave

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,6 +9,8 @@ var isPullingTrigger = false
 var weaponCanFire = bool (true)
 var isPlayerFacingRight = false
 
+var pulseLight = preload("res://Scenes/PulseLight.tscn")
+var shockWave = preload("res://Scenes/ShockWave.tscn")
 
 func _ready():
 	controller = get_node("/root/Controller")
@@ -77,3 +79,28 @@ func _weapon_shoot(motion):
 
 func _weapon_reload_timer_timeout():
     weaponCanFire = true
+
+func _on_Hitbox_body_enter( body ):
+	if(body extends TileMap):
+		var playerRect = self.get_item_rect()
+		var offset
+		# Create pulse & light source
+		if (Input.is_action_pressed("ui_up")):
+			offset = Vector2(0, -1) * (playerRect.size.height/2)
+		if (Input.is_action_pressed("ui_down")):
+			offset = Vector2(0, 1) * (playerRect.size.height/2)
+		if (Input.is_action_pressed("ui_left")):
+			offset = Vector2(-1, 0) * (playerRect.size.width/2)
+		if (Input.is_action_pressed("ui_right")):
+			offset = Vector2(1, 0) * (playerRect.size.width/2)
+		_spawn_shockwave(offset)
+		
+func _spawn_shockwave( offset ):
+	var newWave = shockWave.instance()
+	get_parent().add_child(newWave)
+	newWave.set_pos(get_pos() + offset)
+	newWave.set_scale(Vector2(0.2, 0.2))
+	var newLight = pulseLight.instance()
+	get_parent().add_child(newLight)
+	newLight.set_pos(get_pos() + offset)
+	newLight.set_scale(Vector2(0.2, 0.2))
